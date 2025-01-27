a5f611e0541acca8726f2e04457bfce7982b6bed
marynarzz <marynarzz@proton.me>
2025-01-27 19:58:47 +0100

    fix statusbar

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,9 +137,9 @@ TW_FRAMERATE := 60
 # TWRP (status bar items)
 ifneq ($(OF_HIDE_NOTCH),1)
     TW_STATUS_ICONS_ALIGN := center
-    #TW_CUSTOM_CPU_POS := "300"
+    TW_CUSTOM_CPU_POS := "290"
     TW_CUSTOM_CLOCK_POS := "70"
-    TW_CUSTOM_BATTERY_POS := "900"
+    TW_CUSTOM_BATTERY_POS := "795"
 endif
 
 # TWRP (Crypto)
